32d28d008902f674deea7deace34e9572e79a3c8
Pola03 <paula.silvalara030410@gmail.com>
2024-09-12 11:08:13 -0400

    data fixed

--- src/project.py
+++ src/project.py
@@ -2,9 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
-from keras.layers import Input, Embedding, Concatenate, Dense, GRU, Dropout, Reshape, Masking
+from keras.layers import Input, Embedding, Concatenate, Dense, GRU, Dropout, Reshape, Masking,TimeDistributed
 from keras.models import Model
 import matplotlib.pyplot as plt
+from tensorflow.keras.optimizers import Adam
 
 
 df = pd.read_csv('eCommerce.csv')
@@ -18,7 +19,7 @@ users_multiple_purchases = products_per_user[products_per_user['product_id'] > 1
 df = df[df['user_id'].isin(users_multiple_purchases['user_id'])]
 
 # Encuentra la cantidad máxima de productos que ha comprado un usuario
-max_products_purchased = products_per_user['product_id'].max()
+max_products_purchased = 5
 print(max_products_purchased)
 
 products = df[['product_id', 'brand', 'category_code','price']].drop_duplicates(subset='product_id')
@@ -57,7 +58,7 @@ def generator(min_index, max_index, batch_size=64):
     while True:
         seen_users = set()
         count = 0
-        samples = [np.zeros((batch_size, max_products_purchased - 1)) for _ in range(4)]  
+        samples = [np.zeros((batch_size, max_products_purchased - 1)) for _ in range(4)]
         targets = np.zeros((batch_size, n_products))
         while count < batch_size:
             id = np.random.randint(min_index, max_index)
@@ -72,20 +73,38 @@ def generator(min_index, max_index, batch_size=64):
                 continue
 
             next_purchase = np.random.choice(range(1, len(user_purchases)))
-            purchases = np.array(user_purchases[:next_purchase])
+            # start = 0
+            if next_purchase>max_products_purchased-1:
+                start = next_purchase-max_products_purchased+1
+                purchases = np.array(user_purchases[start:next_purchase])
+            else:
+                purchases = np.array(user_purchases[:next_purchase])
 
-            products_features = products['product_id'].isin(purchases)
+            purchases_df = pd.DataFrame(purchases, columns=['id'])
 
-            x_id = pd.Series(purchases).map(map_prod_id).values
-            x_brand = np.array(products.loc[products_features, 'brand'].map(map_brand_id))
-            x_cat = np.array(products.loc[products_features, 'category_code'].map(map_cat_id))
-            x_price = np.array(products.loc[products_features, 'price'])
+            products_features = pd.merge(purchases_df, products, left_on='id',right_on='product_id', how='left')
+            # print(f'Products features {products_features.size}')
+            
+            x_id = np.array([map_prod_id[prod] for prod in purchases])
+            # print(f'x_id size {x_id.size}')
 
-            if next_purchase + 1 < max_products_purchased:
+            brands_array = np.array(products_features['brand'].values)
+            x_brand = np.array([map_brand_id[brand] for brand in brands_array])
+            # print(f'x_brand.size before {x_brand.size}')
+            
+            cats_array = np.array(products_features['category_code'].values)
+            x_cat = np.array([map_cat_id[cat] for cat in cats_array])
+
+            x_price = np.array(products_features['price'].values)
+
+            # print(f'comparison {len(purchases)} and {max_products_purchased-1}')
+            if len(x_id) < max_products_purchased-1:
                 x_id = np.pad(x_id, (0, max_products_purchased - len(x_id) - 1), 'constant')
                 x_brand = np.pad(x_brand, (0, max_products_purchased - len(x_brand) - 1), 'constant')
                 x_cat = np.pad(x_cat, (0, max_products_purchased - len(x_cat) - 1), 'constant')
                 x_price = np.pad(x_price, (0, max_products_purchased - len(x_price) - 1), 'constant')
+# print('x_brand.size')
+            # print(x_brand.size)
 
             samples[0][count] = x_id
             samples[1][count] = x_brand
@@ -118,7 +137,7 @@ def create_tf_dataset(min_index, max_index, batch_size=64):
 def predict_next_product(user_purchases, model):
     """
     Predice el próximo producto basado en las características de los productos comprados por el usuario.
-    
+
     Args:
     - user_data: Datos del usuario con características de los productos comprados en formato de secuencia.
     - model: El modelo entrenado para hacer la predicción.
@@ -131,11 +150,12 @@ def predict_next_product(user_purchases, model):
     x_brand = np.array(products.loc[products_features,'brand'].map(map_brand_id))
     x_cat = np.array(products.loc[products_features,'category_code'].map(map_cat_id))
     x_price = np.array(products.loc[products_features,'price'])
+    
 
     return model.predict([x_id,x_brand,x_cat,x_price])
 
 
-embedding_dim = 8
+embedding_dim = 16
 # Entrada para el ID
 input_id = Input(shape=(max_products_purchased-1,))
 embedding_id = Embedding(input_dim=n_products+1, output_dim=embedding_dim, mask_zero=True)(input_id)
@@ -148,10 +168,14 @@ embedding_brand = Embedding(input_dim=n_brands+1, output_dim=embedding_dim, mask
 input_cat = Input(shape=(max_products_purchased-1,))
 embedding_cat = Embedding(input_dim=n_categories+1, output_dim=embedding_dim, mask_zero=True)(input_cat)
 
-input_price = Input(shape=(max_products_purchased-1,))
+input_price = Input(shape=(max_products_purchased-1,1))
+price_dense = TimeDistributed(Dense(embedding_dim, activation='relu'))(input_price)
+
+purchases = Concatenate()([embedding_id, embedding_brand, embedding_cat, price_dense])
+
 
 # Concatenar los embeddings
-purchases = Concatenate()([embedding_id, embedding_brand, embedding_cat, Reshape((max_products_purchased-1, 1))(input_price)])
+# purchases = Concatenate()([embedding_id, embedding_brand, embedding_cat, Reshape((max_products_purchased-1, 1))(input_price)])
 
 # Aplicar masking si se usa
 masked_input = Masking(mask_value=0)(purchases)
@@ -160,8 +184,9 @@ dropout_output = Dropout(0.3)(gru_output)
 output = Dense(units=n_products, activation='softmax')(dropout_output)
 
 # Crear el modelo
+optimizer = Adam(learning_rate=0.001)
 model = Model(inputs=[input_id, input_brand, input_cat, input_price], outputs=output)
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
 batch_size = 16
 
@@ -183,7 +208,6 @@ history = model.fit(train_gen,
                     validation_data=val_gen,
                     validation_steps=500)
 
-
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 loss = history.history['loss']
@@ -199,4 +223,3 @@ plt.plot(epochs, val_loss, 'b', label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend()
 plt.show()
-
